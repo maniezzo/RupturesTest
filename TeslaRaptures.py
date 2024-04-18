1ea110a61f64c3c1acf11f87ea512481a86cca2b
Pietro Lelli <pietro.lelli00@gmail.com>
2024-04-18 17:37:28 +0200

    add tesla

--- TeslaRaptures.py
+++ TeslaRaptures.py
@@ -3,7 +3,7 @@ import ruptures as rp
 import matplotlib.pyplot as plt
 import pandas as pd
 
-ds = pd.read_csv('resources/GBP_USDDatiStorici.csv', header=0)
+ds = pd.read_csv('resources/TTSLADati.csv', header=0)
 ds_values = ds.Ultimo.values
 
 # Inverti l'ordine dell'array
@@ -16,28 +16,28 @@ data = np.array([float(x.replace(".", "").replace(",", ".")) for x in data_inver
 #print(data)
 
 # Algoritmo Pelt
-pelt = rp.Pelt(model="l2", jump=2)
-bkps_pelt = pelt.fit_predict(data, 5)
+pelt = rp.Pelt(model="l2", jump=5)
+bkps_pelt = pelt.fit_predict(data, 30)
 
 # Algoritmo BinarySeg
-algo = rp.Binseg(model="l2", jump=2).fit(data)
-bkps_binSeg = algo.predict(pen=5)
+algo = rp.Binseg(model="l2", jump=5).fit(data)
+bkps_binSeg = algo.predict(pen=30)
 
 # Algoritmo Dynp
-algo = rp.Dynp(model="l2", min_size=4, jump=2).fit(data)
+algo = rp.Dynp(model="l2", min_size=3, jump=5).fit(data)
 bkps_dynp = algo.predict(n_bkps=5)
 
 # Visualizzazione dei risultati per Pelt
 rp.display(data, bkps_pelt)
-plt.title("GBP Pelt")
+plt.title("Tesla Pelt")
 plt.show()
 
 # Visualizzazione dei risultati per BinarySeg
 rp.display(data, bkps_binSeg)
-plt.title("GBP BinarySeg")
+plt.title("Tesla BinarySeg")
 plt.show()
 
 # Visualizzazione dei risultati per Dynp
 rp.display(data, bkps_dynp)
-plt.title("GBP Dynp")
+plt.title("Tesla Dynp")
 plt.show()
